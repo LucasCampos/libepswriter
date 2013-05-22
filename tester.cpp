005d26fef4f66fbf26aa35cfb1d427b8cd6a8228
Lucas Campos <lqcc@df.ufpe.br>
2013-05-22 06:06:59 -0300

    Tester updated, more organized

--- tester.cpp
+++ tester.cpp
@@ -1,36 +1,61 @@
 #include "epswriter.hpp"
+#include <cstdlib>
+
+int MyRand(){return rand()%65535;}
 
 int main() {
 
-	epswriter c("circle.eps", -1, -1, 1, 1);
-	epswriter cF("circleF.eps", -1, -1, 1, 1);
+	srand(time(NULL));
 
-	c.circle(0,0,.5,150,0,0);
-	cF.filledCircle(.5,.5,.5,150,0,0);
+	const double xMin = -1, yMin = -1;
+	const double xMax = 1, yMax = 1;
 
-	epswriter t("triangle.eps", -1,-1, 1,1);
-	epswriter tF("triangleFilled.eps", -1, -1, 1,1);
+	{
+		epswriter c("circle.eps", xMin, yMin, xMax, yMax);
+		epswriter cF("circleF.eps", xMin, yMin, xMax, yMax);
+		const double radius = 0.5;
+		const double centerX = 0;
+		const double centerY = 0;
 
-	t.triangle(0, .5, 
-		-.25, -.25, 
-		.25, -.25, 
-		150, 0, 0);
-	tF.filledTriangle(0, .5, -.25, -.25, .25, -.25, 150, 0, 0);
+		c.circle(centerX, centerY,radius,MyRand(), MyRand(), MyRand());
+		cF.filledCircle(centerX, centerY,radius,MyRand(), MyRand(), MyRand());
+	}
 
-	epswriter s("square.eps", -1, -1, 1, 1);
-	epswriter sF("squareFilled.eps", -1, -1, 1, 1);
+	{
+		epswriter t("triangle.eps", xMin, yMin, xMax, yMax);
+		epswriter tF("triangleFilled.eps", xMin, yMin, xMax, yMax);
 
-	s.square(-.5, -.5, .5, .5, 0, 150, 0);
-	sF.filledSquare(-.5, -.5, .5, .5, 0, 150, 0);
+		const double x1= 0  , y1=.5;
+		const double x2=-.25, y2=-.25;
+		const double x3= .25, y3=-.25;
 
-	epswriter l("line.eps", -1, -1, 1, 1);
-	epswriter ml("multiline.eps",-1,-1,1,1);
+		t.triangle(x1,y1,x2,y2,x3,y3,MyRand(),MyRand(),MyRand());
+		tF.filledTriangle(x1,y1,x2,y2,x3,y3,MyRand(),MyRand(),MyRand());
+	}
 
-	std::vector<double> x,y;
-	x.push_back(.5);x.push_back(.7); x.push_back(.9);
-	y.push_back(-.3);y.push_back(.4); y.push_back(0);
-	
-	l.line(-1,-1, .5, 1.0, 0.05, 0, 140, 230);
-	ml.multiline(x,y, 0.05, 240, 120, 10);
+	{
+		epswriter s("square.eps", xMin, yMin, xMax, yMax);
+		epswriter sF("squareFilled.eps", xMin, yMin, xMax, yMax);
+			
+		const double leftDownX = -.5, leftDownY=-.5;
+		const double rightUpX = .5, rightUpY = .5;
+		s.square(leftDownX, leftDownY, rightUpX, rightUpY,MyRand(),MyRand(),MyRand());
+		sF.filledSquare(leftDownX, leftDownY, rightUpX, rightUpY,MyRand(),MyRand(),MyRand());
+	}
+
+	{
+		epswriter l("line.eps", xMin, yMin, xMax, yMax);
+		epswriter ml("multiline.eps",xMin, yMin, xMax, yMax);
+
+		const double lineWidth = .05;
+		const double x0 = -1, y0=-1;
+		const double xf = .5, yf=1.0;
+		std::vector<double> x,y;
+		x.push_back(.5);x.push_back(.7); x.push_back(.9);
+		y.push_back(-.3);y.push_back(.4); y.push_back(0);
+
+		l.line(x0,y0,xf,yf, lineWidth, MyRand(),MyRand(),MyRand());
+		ml.multiline(x,y, lineWidth, MyRand(),MyRand(),MyRand());
+	}
 
 }
